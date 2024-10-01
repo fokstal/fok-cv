d0562c6f71f0ecee41f022ee55ef0414783846bb
Fokstal <klabu2016@mail.ru>
2024-10-01 20:11:43 +0300

    feat: add localization for about page (Tue 01.10.24 08:11 PM)

--- scripts/const/translation.js
+++ scripts/const/translation.js
@@ -1,19 +1,39 @@
 const translation = {
     BY: {
-        exp: "вопыт",
-        about: "пра мяне",
-        contact: "кантакты",
+        // home page
         helloText: `
             Прывітанне, свет!<br>
             Мяне клічуць <strong>Кірыл</strong><br>
             і я <strong>.NET разработчык!</strong><br>
         `,
-        writeMe: "Напiсаць мне",
-        nameForm: "Iмя *",
+
+        // exp page
+        exp: "вопыт",
+
+        // about page
+        about: "пра мяне",
+        startTitle: "Мяне завуць Кірыл Лабуць, Прывітанне!",
+        startText: "Lorem іpsum dolor sіt amet",
+        educationTitle: "Адукацыя",
+        educationText: "Lorem іpsum dolor sіt amet",
+        hobbyTitle: "Заняткі",
+        hobbySoccer: "я гуляю ў футбол",
+        hobbyBasketball: "таксама ў баскетбол",
+        hobbyAnyBall: "ў любыя гульні з мячом...",
+        hobbyLoveSport: "Я люблю спорт!",
+        hobbyAny: "Актыўна чытаю кнігі і смачна гатую",
+        hobbyMe: "гэта я",
+
+        // contact page
+        contact: "кантакты",
+        writeMe: "Напісаць мне",
+        nameForm: "Імя *",
         emailForm: "Электронная пошта *",
         messageForm: "Паведамленне *",
-        btnForm: "напiсаць",
+        btnForm: "напісаць",
         moreLinks: "больш спасылак",
+
+        // tіtle
         title: {
             shareLink: "Падзяліцца спасылкай на старонку",
             changeLanguage: "Змяніць мову",
@@ -21,20 +41,40 @@ const translation = {
         },
     },
     RU: {
-        exp: "опыт",
-        about: "обо мне",
-        contact: "контакты",
+        // home page
         helloText: `
             Приветствую, мир!<br>
             Меня зовут <strong>Кирилл</strong><br>
             и я <strong>.NET Разработчик!</strong><br>
         `,
+
+        // exp page
+        exp: "опыт",
+
+        // about page
+        about: "обо мне",
+        startTitle: "Меня зовут Кирилл Лабуть, Привет!",
+        startText: "Lorem ipsum dolor sit amet",
+        educationTitle: "Образование",
+        educationText: "Lorem ipsum dolor sit amet",
+        hobbyTitle: "Хобби",
+        hobbySoccer: "я играю в футбол",
+        hobbyBasketball: "и в баскетбол",
+        hobbyAnyBall: "в любые игры с мячом...",
+        hobbyLoveSport: "Я люблю спорт!",
+        hobbyAny: "Активно читаю книги и вкусно готовлю",
+        hobbyMe: "это я",
+
+        // contact page
+        contact: "контакты",
         writeMe: "Написать мне",
         nameForm: "Имя *",
         emailForm: "Электронная почта *",
         messageForm: "Сообщение *",
         btnForm: "написать",
         moreLinks: "больше ссылок",
+
+        // title
         title: {
             shareLink: "Поделиться ссылкой на страницу",
             changeLanguage: "Изменить язык",
@@ -42,20 +82,40 @@ const translation = {
         },
     },
     EN: {
-        exp: "exp",
-        about: "about",
-        contact: "contact",
+        // home page
         helloText: `
             Hello world!<br>
             My name is <strong>Kirill</strong><br>
             and I am a <strong>.NET Developer!</strong><br>
         `,
+        
+        // exp page
+        exp: "exp",
+
+        // about page
+        about: "about",
+        startTitle: "I`am Kirill Labut, Hi!",
+        startText: "Lorem ipsum dolor sit amet",
+        educationTitle: "Education",
+        educationText: "Lorem ipsum dolor sit amet",
+        hobbyTitle: "Hobby",
+        hobbySoccer: "i play in soccer",
+        hobbyBasketball: "and basketball",
+        hobbyAnyBall: "any ball game...",
+        hobbyLoveSport: "i love the <strong>sport!</strong>",
+        hobbyAny: "I am actively trying to make books and cook delicious",
+        hobbyMe: "it`s me",
+
+        // contact page
+        contact: "contact",
         writeMe: "Write me",
         nameForm: "Name *",
         emailForm: "Email *",
         messageForm: "Message *",
         btnForm: "write",
         moreLinks: "take more links",
+
+        // title
         title: {
             shareLink: "Share link on site",
             changeLanguage: "Change language",
