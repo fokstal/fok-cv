850503d861efe03b7be506a229b7fad81f6b8cdc
Fokstal <klabu2016@mail.ru>
2024-10-27 12:41:19 +0300

    refactor (contact page): manage HTML, CSS and JS by BEM (Sun 27.10.24 12:41 PM)

--- scripts/service/email.js
+++ scripts/service/email.js
@@ -1,10 +1,10 @@
 const sendMessageToEmail = () => {
-    document.querySelector("#pigeonImg").classList.toggle("rotate");
+    document.querySelector(".contact__write-message-block-image").classList.toggle("rotate");
 
-    document.querySelectorAll("input").forEach(inputBox => {
-        inputBox.value = null;
+    document.querySelectorAll(".contact__write-message-block-form-item-text-box").forEach(textBox => {
+        textBox.value = null;
     });
-    document.querySelector("textarea").value = null;
+    document.querySelector(".contact__write-message-block-form-item-text-box-area").value = null;
 }
 
 export default sendMessageToEmail;
