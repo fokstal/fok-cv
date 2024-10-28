8418cdaf5ddf8ad074c8bc86c4935726ef7e3566
Fokstal <klabu2016@mail.ru>
2024-10-28 21:05:04 +0300

    fix: correct animation in pigeon image by send message (Mon 28.10.24 09:04 PM)

--- scripts/service/email.js
+++ scripts/service/email.js
@@ -1,10 +1,14 @@
 const sendMessageToEmail = () => {
-    document.querySelector(".contact__write-message-block-image").classList.toggle("rotate");
+    document.querySelector(".contact__write-message-block-image").classList.add("element-rotate");
 
     document.querySelectorAll(".contact__write-message-block-form-item-text-box").forEach(textBox => {
         textBox.value = null;
     });
     document.querySelector(".contact__write-message-block-form-item-text-box-area").value = null;
+
+    setTimeout(() => {
+        document.querySelector(".contact__write-message-block-image").classList.remove("element-rotate");
+    }, 1000);
 }
 
 export default sendMessageToEmail;
