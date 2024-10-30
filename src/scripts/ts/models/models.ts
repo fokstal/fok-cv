4dec914ecc2885b2008bb7717f4f082d3e90b8dc
Fokstal <klabu2016@mail.ru>
2024-10-30 21:56:40 +0300

    feat: add type for PageList (Wed 30.10.24 09:56 PM)

--- src/scripts/ts/models/models.ts
+++ src/scripts/ts/models/models.ts
@@ -1,6 +1,12 @@
-interface ComponentsModel {
+import { PAGE_NAME_ENUM } from "../const/const"
+
+type PageListModel = {
+    [key in PAGE_NAME_ENUM]: HTMLElement;
+}
+
+type ComponentsModel = {
     header: HTMLElement,
-    pageList: HTMLElement[],
+    pageList: PageListModel,
     footer: HTMLElement,
     overlay: HTMLElement,
     imgModal: HTMLElement,
