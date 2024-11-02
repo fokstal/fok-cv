8274aec49726d79ed9558581a6040261f2b21a14
Fokstal <klabu2016@mail.ru>
2024-11-02 13:17:26 +0300

    feat (email worker): convert JS to TS (Sat 02.11.24 01:17 PM)

---
+++ src/scripts/ts/service/functions/sendMessageToEmail.ts
@@ -0,0 +1,20 @@
+import { getElementFromDocument, getElementListFromDocument } from "../../helpers/elements";
+
+const sendMessageToEmail = () => {
+    const writeMessageBlockImg: HTMLImageElement = getElementFromDocument<HTMLImageElement>(".contact__write-message-block-image");
+
+    writeMessageBlockImg.classList.add("element-rotate");
+
+    getElementListFromDocument<HTMLInputElement>(".contact__write-message-block-form-item-text-box")
+        .forEach(textBox => {
+            textBox.value = "";
+        });
+
+    getElementFromDocument<HTMLInputElement>(".contact__write-message-block-form-item-text-box-area").value = "";
+
+    setTimeout(() => {
+        writeMessageBlockImg.classList.remove("element-rotate");
+    }, 1000);
+}
+
+export default sendMessageToEmail;
