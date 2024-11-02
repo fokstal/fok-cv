9af093f54b65f2b90916229cd27f0bc87e9fb497
Fokstal <klabu2016@mail.ru>
2024-11-02 12:21:39 +0300

    feat (access mode service): convert JS to TS (Sat 02.11.24 12:21 PM)

---
+++ src/scripts/ts/service/AccessModeWorker.ts
@@ -0,0 +1,43 @@
+import { STORAGE_KEYS, FOLDERS, ICON_FULLNAMES } from "../const/const.js";
+import { getElementFromDocument } from "../helpers/elements.js";
+
+class AccessModeWorker {
+    accessModeImgEl: HTMLImageElement;
+    isAccessModeBaseValue: boolean = false;
+    private _currentIsAccessMode: boolean = this.isAccessModeBaseValue;
+
+    get currentIsAccessMode() {
+        return this._currentIsAccessMode;
+    }
+
+    set currentIsAccessMode(isAccessMode: boolean) {
+        this._currentIsAccessMode = isAccessMode;
+        sessionStorage.setItem(STORAGE_KEYS.session.isAccessMode, String(isAccessMode));
+    }
+
+    get currentIconPath() {
+        let iconName = this.currentIsAccessMode ? ICON_FULLNAMES.eye : ICON_FULLNAMES.eyeClosed;
+
+        return window.location.origin + FOLDERS.icons.any + iconName;
+    }
+
+    constructor(accessModeImgElSelector: string, isAccessModeBaseValue: boolean = false) {
+        this.accessModeImgEl = getElementFromDocument<HTMLImageElement>(accessModeImgElSelector);
+        this.isAccessModeBaseValue = isAccessModeBaseValue;
+
+        this.currentIsAccessMode = sessionStorage.getItem(STORAGE_KEYS.session.isAccessMode) === "true" || isAccessModeBaseValue;
+        this.accessModeImgEl.src = this.currentIconPath;
+    }
+
+    toggleAccessMode = () => {
+        this.currentIsAccessMode = !this.currentIsAccessMode;
+
+        this.accessModeImgEl.classList.add("access-mode-button-icon--fade-out");
+        this.accessModeImgEl.addEventListener("transitionend", () => {
+            this.accessModeImgEl.src = this.currentIconPath;
+            this.accessModeImgEl.classList.remove("access-mode-button-icon--fade-out");
+        }, { once: true });
+    };
+}
+
+export default AccessModeWorker;
