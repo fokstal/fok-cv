3939e3d14be2bbaabbe4e26c92a0e23a880c49f5
Fokstal <klabu2016@mail.ru>
2024-11-18 07:42:08 +0300

    feat: change chart option and change type of chart (Mon 18.11.24 07:42 AM)

--- src/scripts/const/chartOptions/commitFreq_ChartOption.ts
+++ src/scripts/const/chartOptions/commitFreq_ChartOption.ts
@@ -1,20 +1,66 @@
 import { EChartOption } from "echarts";
+import getRandomHEXColor from "@scripts/helpers/getRandomHEXColor";
+
+const colorGradientStart = getRandomHEXColor();
+const colorGradientEnd = getRandomHEXColor();
 
 const commitFreq_ChartOption: EChartOption = {
+    tooltip: {
+        trigger: "axis",
+        axisPointer: {
+            type: "cross",
+        },
+    },
+    // toolbox: {
+    //     show: true,
+    //     feature: {
+    //         saveAsImage: {}
+    //     }
+    // },
     xAxis: {
         type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        data: ["2021", "2022", "2023", "2024", "2025"],
     },
     yAxis: {
-        type: "value"
+        type: "value",
+        max: 200,
     },
     series: [
         {
-            data: [120, 200, 150, 80, 70, 110, 130],    
-            type: "bar"
+            itemStyle: {
+                color: {
+                    type: "linear",
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [{
+                        offset: 0, color: colorGradientStart,
+                    }, {
+                        offset: 1, color: colorGradientEnd,
+                    }],
+                    global: false,
+                }
+            },
+            areaStyle: {
+                color: {
+                    type: "linear",
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [{
+                        offset: 0, color: colorGradientStart + "30",
+                    }, {
+                        offset: 1, color: colorGradientEnd + "30",
+                    }],
+                    global: false,
+                },
+            },
+            data: [0, 5, 96, 109],
+            type: "line"
         }
     ]
 };
-
 
 export default commitFreq_ChartOption;
