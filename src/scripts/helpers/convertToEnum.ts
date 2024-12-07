37553599ad18bbbd42288f8578d0d5f487d5ea0e
Fokstal <klabu2016@mail.ru>
2024-12-07 18:16:37 +0300

    refactor: change Error message (Sat 07.12.24 06:16 PM)

--- src/scripts/helpers/convertToEnum.ts
+++ src/scripts/helpers/convertToEnum.ts
@@ -7,14 +7,14 @@ const convertStringToLanguageEnum = (line: string): LANGUAGE_ENUM => {
         }
     }
 
-    throw new Error("This LANGUAGE is not supported!");
+    throw new Error(`LANGUAGE '${line}' is not supported!`);
 }
 
 const convertStringToPageNameEnum = (line: string): PAGE_NAME_ENUM => {
     if (line in PAGE_NAME_ENUM)
         return PAGE_NAME_ENUM[line as keyof typeof PAGE_NAME_ENUM];
 
-    throw new Error("This PAGE_NAME is not supported!");
+    throw new Error(`PAGE_NAME ${line} is not supported!`);
 }
 
 export { convertStringToLanguageEnum, convertStringToPageNameEnum };
