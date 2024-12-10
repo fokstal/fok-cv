de55fb3d81641414cb1a2ca544aef5db1c6d08c5
Fokstal <klabu2016@mail.ru>
2024-12-10 19:58:28 +0300

    feat: add abstract method for get elements by selecotor list (Tue 10.12.24 07:58 PM)

--- src/scripts/service/Layout.ts
+++ src/scripts/service/Layout.ts
@@ -1,3 +1,5 @@
+import { getElementFromDocument } from "@scripts/helpers/elements";
+
 class Layout {
     public static getChartTooltip = (bodyLayout: string, titleText?: string): string => {
         const titleEl = document.createElement("h3");
@@ -12,6 +14,20 @@ class Layout {
         bodyEl.style.textAlign = "left";
 
         return titleEl.textContent ? `${titleEl.outerHTML}<br>${bodyEl.outerHTML}` : bodyEl.outerHTML;
+    }
+
+    public static getElementsBySelectorList
+        <TElementList extends Record<string, HTMLElement>, TSelectorList extends Record<string, string>>
+        (selectorList: TSelectorList): TElementList {
+        const elements: Partial<TElementList> = {} as TElementList;
+
+        for (const selector in selectorList) {
+            if (selectorList.hasOwnProperty(selector)) {
+                elements[selector] = getElementFromDocument<HTMLElement>(selectorList[selector]) as TElementList[typeof selector];
+            }
+        }
+
+        return elements as TElementList;
     }
 }
 
